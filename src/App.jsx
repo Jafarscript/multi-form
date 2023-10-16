bc837f262279009c5637279d1f3fd5035a146dae
Jafarscript <lihammedjafar@gmail.com>
2023-10-17 00:58:04 +0100

    Implementation of the design has started

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css';
-import Sidebar from './components/Sidebar';
+import "./App.css";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="App bg-magnolia h-[100vh] flex justify-center align-middle">
-      <form className='bg-lol-white flex align-middle justify-center'>
+    <div className="App bg-magnolia flex justify-center items-center min-h-[80vh]">
+      <section>
         <Sidebar />
-      </form>
+      </section>
     </div>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,49 @@
-import React from 'react'
+import React from "react";
 
 const Sidebar = () => {
   return (
-    <div className='bg-purplish-blue'>
-      <ul>
-        <li>
-          <h5>Step 1</h5>
-          <h3>YOUR INFO</h3>
+    <div className="bg-purplish-blue pt-10">
+      <ul className="text-lol-white flex gap-5 flex-col ml-10">
+        <li className="flex gap-5 items-center">
+          <span className="border rounded-[50%] border-solid border-white py-2 px-4">
+            1
+          </span>
+          <p>
+            <h6>Step 1</h6>
+            <h3>YOUR INFO</h3>
+          </p>
         </li>
-        <li></li>
+        <li className="flex gap-5 items-center">
+          <span className="border rounded-[50%] border-solid border-white py-2 px-4">
+            2
+          </span>
+          <p>
+            <h6>Step 2</h6>
+            <h3>SELECT PLAN</h3>
+          </p>
+        </li>
+        <li className="flex gap-5 items-center">
+          <span className="border rounded-[50%] border-solid border-white py-2 px-4">
+            3
+          </span>
+
+          <p>
+            <h6>Step 3</h6>
+            <h3>ADD-ONS</h3>
+          </p>
+        </li>
+        <li className="flex gap-5 items-center">
+          <span className="border rounded-[50%] border-solid border-white py-2 px-4">
+            4
+          </span>
+          <p>
+            <h6>Step 4</h6>
+            <h3>SUMMARY</h3>
+          </p>
+        </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
